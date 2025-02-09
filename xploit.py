320f529220b15bfdd3c28bcbb16bd0984b18f0e4
FuadKazimov <kazimovfuad02@gmail.com>
2025-02-09 15:40:45 +0400

    ilk

---
+++ xploit.py
@@ -0,0 +1,15 @@
+qiymetler = {
+    "Riyaziyyat": 85,
+    "Fizika": 78,
+    "Kimya": 90,
+    "İngilis dili": 88,
+    "Tarix": 76
+}
+cem = 0
+say = 0
+
+for qiymet in qiymetler.values():
+    cem += qiymet
+    say += 1
+orta_qiymet = cem / say
+print(f"Telebenin orta qiymeti: {round(orta_qiymet)}")
